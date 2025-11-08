1f8ee10b8fd5de8c0a59bfcc29a378bbd4446f84
Henrique Walker <Joaneshwc31@gmail.com>
2025-11-07 23:22:30 -0400

    feat(frontend): adicionar header e botão de voltar

--- php/livro-create.php
+++ php/livro-create.php
@@ -11,6 +11,22 @@
 
 <body>
   <?php include('navbar.php'); ?>
+  <div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card bg-white text-dark border-0 shadow">
+                <div class="card-header bg-dark text-light">
+                    <h4>Adicionar Livro
+                        <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
