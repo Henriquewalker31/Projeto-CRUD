999da2945b4ddf5146ddd503cee17c2d224d1dbf
Henrique Walker <Joaneshwc31@gmail.com>
2025-11-07 21:27:50 -0400

    feat(frontend): adicionar navbar e layout inicial da página de livros

--- php/navbar.php
+++ php/navbar.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1A374D;">
     <div class="container-md">
-        <a class = "navbar-brand" href="#">LIVRARIA </a>
+        <a class = "navbar-brand" href="#">Sistema de Livraria </a>
     </div>
 </nav>
