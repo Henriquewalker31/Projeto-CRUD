e25556ead99fd552d34d0aaae09b299327e4a501
Henrique Walker <Joaneshwc31@gmail.com>
2025-11-08 01:01:43 -0400

    feat: adicionar funcionalidade para criar livros no banco de dados

---
+++ php/acoes.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require 'conexao.php';
+
+if (isset($_POST['livro_create'])) {
+    $titulo = mysqli_real_escape_string($conn, trim($_POST['titulo']));
+    $autor = mysqli_real_escape_string($conn, trim($_POST['autor']));
+    $ano_publicacao = mysqli_real_escape_string($conn, trim($_POST['ano_publicacao']));
+    $editora = mysqli_real_escape_string($conn, trim($_POST['editora']));
+    $categoria = mysqli_real_escape_string($conn, trim($_POST['categoria']));
+    $quantidade = mysqli_real_escape_string($conn, trim($_POST['quantidade']));
+    $descricao = mysqli_real_escape_string($conn, trim($_POST['descricao']));
+    $data_cadastro = mysqli_real_escape_string($conn, trim($_POST['data_cadastro']));
+
+
+    $sql = "INSERT INTO livros (titulo, autor, ano_publicacao, editora, categoria, quantidade, descricao, data_cadastro)
+      VALUES ('$titulo', '$autor','$ano_publicacao','$editora','$categoria','$quantidade','$descricao','$data_cadastro')";
+
+    if (mysqli_query($conn, $sql)) {
+        // Livro adicionado com sucesso → redireciona
+        header("Location: index.php");
+        exit(); // importante
+    } else {
+        echo "Erro ao adicionar livro: " . mysqli_error($conn);
+    }
+}
+?>
