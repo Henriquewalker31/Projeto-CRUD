16efe7873fb6d1ea8286d88b18d38a219b3b99d5
Henrique Walker <Joaneshwc31@gmail.com>
2025-11-08 22:59:17 -0400

    Mudanças na navbar

--- php/navbar.php
+++ php/navbar.php
@@ -1,5 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1A374D;">
-    <div class="container-md">
-        <a class = "navbar-brand" href="#">Sistema de Livraria </a>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-dark shadow-sm py-3"
+    style="background: linear-gradient(90deg, #1A374D, #406882);">
+<div class="container-md">
+    <a class="navbar-brand fw-bold m-0" href="#">📚 Sistema de Livraria </a>
+</div>
 </nav>
