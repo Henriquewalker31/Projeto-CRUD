218598ec5da3b157cf69cecdbc968cc3a06cc3f1
Henrique Walker <Joaneshwc31@gmail.com>
2025-11-14 21:54:44 -0400

    Remove estrutura antiga: JS, css, php antigas

--- php/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-dark shadow-sm py-4"
-    style="background: #1B3B6F; ">
-    <div class="container-fluid">
-        <a class="navbar-brand fw-bold m-0 ms-3" href="#">📚 Sistema de Livraria </a>
-    </div>
-</nav>
-<!-- atualização para commit -->
